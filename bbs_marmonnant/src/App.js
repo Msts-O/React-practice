b00acc952c23babaaf3abc57768bc6561f489e3d
Masatoshi <msts.oo0131@gmail.com>
2020-12-03 21:37:49 +0900

    route_compile

--- bbs_marmonnant/src/App.js
+++ bbs_marmonnant/src/App.js
@@ -1,11 +1,16 @@
-import './App.css';
+import React, {Component} from 'react';
+import {BrowserRouter} from 'react-router-dom';
 
-function App() {
-    return(
-       <div ClassName="App">
-          <h1>Marmonnant</h1>
-        </div>
-    );
+class App extends Component{
+    render() {
+     return(
+       <BrowserRouter>
+         <div classname="App">
+            <h1>Marmonnant</h1>
+            </div>
+        </BrowserRouter>
+     )
+    }
+
 }
-
 export default App;
