484f20a3216f74449ff6e690a2688b6c21bc4e84
Masatoshi <msts.oo0131@gmail.com>
2020-12-04 15:08:38 +0900

    pathの初段会の指定(TopPage表記完了)

--- bbs_marmonnant/src/App.js
+++ bbs_marmonnant/src/App.js
@@ -1,16 +1,16 @@
 import React, {Component} from 'react';
 import {BrowserRouter} from 'react-router-dom';
+import Navbar from './Components/Navbar';
 
 class App extends Component{
     render() {
      return(
        <BrowserRouter>
          <div classname="App">
-            <h1>Marmonnant</h1>
+            <Navbar/>
             </div>
         </BrowserRouter>
-     )
+     );
     }
-
 }
 export default App;
