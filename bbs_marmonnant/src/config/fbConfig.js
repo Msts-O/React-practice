9710e6f061e4bd24effd69dc816cbb5c07457c65
Masatoshi <msts.oo0131@gmail.com>
2020-12-10 19:13:32 +0900

    objectの活性化以外はコーディング完了

--- bbs_marmonnant/src/config/fbConfig.js
+++ bbs_marmonnant/src/config/fbConfig.js
@@ -1,9 +1,15 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
-
 // Your web app's Firebase configuration
-
+var firebaseConfig = {
+    apiKey: "AIzaSyAOdNwwzi6B6vtte1hACpeR_SFhWEjvoeY",
+    authDomain: "marmonnant.firebaseapp.com",
+    projectId: "marmonnant",
+    storageBucket: "marmonnant.appspot.com",
+    messagingSenderId: "1307793185",
+    appId: "1:1307793185:web:6109444fdfcda337801060"
+};
 
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
--- bbs_marmonnant/src/store/actions/projectActions.js
+++ bbs_marmonnant/src/store/actions/projectActions.js
@@ -1,5 +1,17 @@
 export const createProject = (project) => {
   return(dispatch, getState, { getFirebase, getFirestore }) =>{
-      dispatch({ type: 'Create_Project',project})
+      const firestore = getFirestore();
+      // eslint-disable-next-line
+       firestore.collection('projects') .add({
+            ...project,
+           authorFirstname: 'Masa',
+           authorLastname:  'Ogi',
+           authorId:12345,
+           createdAt: new Date()
+           }).then(() =>{
+              dispatch({ type: 'CREATE_PROJECT',project})
+           }).catch((err) => {
+               dispatch({ type: 'CREATE_PROJECT_ERROR',err})
+       })
   }
 };
