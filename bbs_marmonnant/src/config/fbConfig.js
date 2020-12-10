c9d23c542386d0abf1a6b43202573f916460fe5f
Masatoshi <msts.oo0131@gmail.com>
2020-12-10 16:27:48 +0900

    firebase_db_connect完了

--- bbs_marmonnant/src/config/fbConfig.js
+++ bbs_marmonnant/src/config/fbConfig.js
@@ -3,40 +3,10 @@ import 'firebase/firestore'
 import 'firebase/auth'
 
 // Your web app's Firebase configuration
-var firebaseConfig = {
-    apiKey: "AIzaSyCZItfqmvg5jjK0JXR9cnoW9xcHmPLWkfs",
-    authDomain: "net-ninja-marioplan-178f9.firebaseapp.com",
-    databaseURL: "https://net-ninja-marioplan-178f9.firebaseio.com",
-    projectId: "net-ninja-marioplan-178f9",
-    storageBucket: "net-ninja-marioplan-178f9.appspot.com",
-    messagingSenderId: "240847046405",
-    appId: "1:240847046405:web:45a4511e8e31622089196c",
-    measurementId: "G-BJYQ8GHCBL"
-};
+
+
 // Initialize Firebase
 firebase.initializeApp(firebaseConfig);
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-firebase.firestore()
-
-export default firebase;import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/writer'
-
-var firebaseConfig = {
-    apiKey: "AIzaSyCZItfqmvg5jjK0JXR9cnoW9xcHmPLWkfs",
-    authDomain: "net-ninja-marioplan-178f9.firebaseapp.com",
-    databaseURL: "https://net-ninja-marioplan-178f9.firebaseio.com",
-    projectId: "net-ninja-marioplan-178f9",
-    storageBucket: "net-ninja-marioplan-178f9.appspot.com",
-    messagingSenderId: "240847046405",
-    appId: "1:240847046405:web:45a4511e8e31622089196c",
-    measurementId: "G-BJYQ8GHCBL"
-};
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
 firebase.firestore()
 
 export default firebase;
