d6db7067dc95b46c3d6e095f8cc92a9b182568ef
Masatoshi <msts.oo0131@gmail.com>
2020-12-14 19:26:37 +0900

    login_base_commit

--- bbs_marmonnant/src/store/actions/projectActions.js
+++ bbs_marmonnant/src/store/actions/projectActions.js
@@ -1,4 +1,4 @@
-export const createProject = (project) => {
+    export const createProject = (project) => {
   return(dispatch, getState, { getFirebase, getFirestore }) =>{
       const firestore = getFirestore();
       // eslint-disable-next-line
